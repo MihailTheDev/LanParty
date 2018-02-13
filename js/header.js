eb799bf282a2d9106fc08b36694870431d6ba75b
IsTreaz <eduardtibuleac@gmail.com>
2018-02-13 19:17:14 +0200

    incepem responsive

--- js/header.js
+++ js/header.js
@@ -1,8 +1,8 @@
 function myFunction() {
     var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
+    if (x.className === "topnav text-center mx-auto") {
         x.className += " responsive";
     } else {
-        x.className = "topnav";
+        x.className = "topnav text-center mx-auto";
     }
 }
